bf5a8f852bb33acf4ca587d34da093165aa9c2c0
Bartosz <bartosz.krol@bnpparibas.pl>
2026-04-17 21:02:57 +0200

    setup spring project

---
+++ src/main/java/pl/edu/pjatk/mas/ztm/ZTMApplication.java
@@ -0,0 +1,11 @@
+package pl.edu.pjatk.mas.ztm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZTMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZTMApplication.class, args);
+    }
+}
